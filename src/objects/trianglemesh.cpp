2bb0568f808cc622806e29b85e20a473f707cb89
Andrew Yaroshevych <ayaroshlvua@gmail.com>
2023-04-29 19:12:53 +0300

    Reduced the number of `normalize` calls

--- src/objects/trianglemesh.cpp
+++ src/objects/trianglemesh.cpp
@@ -74,13 +74,12 @@ bool TriangleMesh::testIntersections(const Ray &cast_ray, glm::vec3 &int_point, 
         glm::vec3 tri_int_point, tri_loc_normal;
         if (tri->testIntersections(local_ray, tri_int_point, tri_loc_normal)) {
             tri_int_point = transformation_m.applyTransform(tri_int_point, Direction::FORWARD);
-            tri_loc_normal = glm::normalize(transformation_m.applyTransform(tri_loc_normal, Direction::FORWARD));
             float dist = glm::distance(cast_ray.getOrigin(), tri_int_point);
 
             if (dist < closest_hit) {
                 closest_hit = dist;
                 int_point = tri_int_point;
-                loc_normal = tri_loc_normal;
+                loc_normal = glm::normalize(transformation_m.applyTransform(tri_loc_normal, Direction::FORWARD));
                 loc_color = base_color_m;
                 hit = true;
             }
